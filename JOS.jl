74769e96851b65e4dea905b39def66c1b06ef2b6
ruinzinho <ruinzinho_1991@hotmail.com>
2019-05-16 01:17:08 +0100

    Refactoring and fixed some inheritance problems

--- JOS.jl
+++ JOS.jl
@@ -1,83 +1,92 @@
-struct IntrospectableClass
+struct Class
     name
-    inheritance
+    super
     slots
 end
 
-mutable struct IntrospectableInstance
-    className
-    slotsValues
+struct Instance
+    class
+    slots_values
 end
 
-mutable struct IntrospectableFunction
-    name
-    parameters
-    body
-    native_function
-end
-
-function make_class(className, inheritance, slots)
-    IntrospectableClass(className,
-                        inheritance,
-                        slots)
-end
-
-function make_instance(className, receivingSlotsValues...)
-    #TODO: estruturar melhor
-    slotsValues = Dict()
-    receivingSlotsValues = Dict(receivingSlotsValues)
-
-    for slot in className.slots
-        slotsValues[slot] = nothing
+function make_class(name, super, slots)
+    s = append!([], slots)
+    for class in super
+        append!(s, class.slots)
     end
 
-    for inheritance in className.inheritance
-        for slot in inheritance.slots
-            slotsValues[slot] = nothing
+    Class(name, super, tuple(s...))
+end
+
+#C1 = make_class(:C1, [], [:a])
+#C2 = make_class(:C2, [], [:b, :c])
+#C3 = make_class(:C3, [C1, C2], [:d])
+
+macro defclass(name, super, slots...)
+    :($(esc(name)) = make_class($(QuoteNode(name)), $super, $slots))
+end
+
+#@defclass(C1, [], a)
+#@defclass(C2, [], b, c)
+#@defclass(C3, [C1, C2], d)
+
+function make_instance(class, values...)
+    values = Dict(values)
+    slots_values = Dict{Any, Any}(zip(class.slots, fill(nothing, length(class.slots))))
+    for (slot, value) in values
+        if haskey(slots_values, slot)
+            slots_values[slot] = value
+        else
+            println("ERROR: Slot $(slot) is missing")
         end
     end
-    for slot in slotsValues
-        if slot.first in keys(receivingSlotsValues)
-            slotsValues[slot.first] = receivingSlotsValues[slot.first]
-        end
-    end
 
-    IntrospectableInstance(className, slotsValues)
+    Instance(class, slots_values)
 end
 
-function get_slot(className, slotName)
-    dictSlots = getfield(className, :slotsValues)
-    if slotName in keys(dictSlots)
-        dictSlots[slotName] === nothing ?
-            println("ERROR: Slot $slotName is unbound") : dictSlots[slotName]
+#c3i1 = make_instance(C3, :a=>1, :b=>2, :c=>3, :d=>4)
+#c3i2 = make_instance(C3, :b=>2)
+
+function get_slot(instance, slot)
+    slots_values = getfield(instance, :slots_values)
+    if haskey(slots_values, slot)
+        value = slots_values[slot]
+        value === nothing ? println("ERROR: Slot $(slot) is unbound") : value
     else
-        println("ERROR: Slot $slotName is missing")
+        println("ERROR: Slot $(slot) is missing")
     end
 end
 
-
-function set_slot!(className, slotName, value)
-    dictSlots = getfield(className, :slotsValues)
-    if slotName in keys(dictSlots)
-        dictSlots[slotName] = value
+function set_slot!(instance, slot, value)
+    slots_values = getfield(instance, :slots_values)
+    if haskey(slots_values, slot)
+        slots_values[slot] = value
     else
-        println("ERROR: Slot $slotName is missing")
+        println("ERROR: Slot $slot is missing")
     end
 end
 
-function Base.getproperty(instance::IntrospectableInstance, slotName::Symbol)
-    get_slot(instance, slotName)
+#get_slot(c3i2, :b)
+#set_slot!(c3i2, :b, 3)
+#println([get_slot(c3i1, s) for s in [:a, :b, :c]])
+
+function Base.getproperty(instance::Instance, slot::Symbol)
+    get_slot(instance, slot)
 end
 
-function Base.setproperty!(instance::IntrospectableInstance, slotName::Symbol, value)
-    set_slot!(instance, slotName, value)
+function Base.setproperty!(instance::Instance, slot::Symbol, value)
+    set_slot!(instance, slot, value)
 end
 
-macro defclass(className, inheritance, slots...)
-    :($(esc(className)) = make_class($(QuoteNode(className)), $inheritance, $slots))
-end
+#c3i1.a
+#c3i1.e
+#c3i2.a
+#c3i2.a = 5
+#c3i2.a
 
-@macroexpand @defclass(C5, [], a)
+#############
+# Functions #
+#############
 
 function make_generic(arguments...)
 
@@ -99,21 +108,9 @@ macro defmethod(expr)
     :($(esc(name)) = make_method($(esc(name)), $parameters, $body))
 end
 
-@macroexpand @defmethod foo(c::C1) = 1
-@defmethod foo(c::C1) = 1
-make_method(:foo, :(c::IntrospectableClass,), 1)
+#@defgeneric foo(c)
+#@defmethod foo(c::C1) = 1
+#@defmethod foo(c::C2) = c.b
 
-C1 = make_class(:C1, [], [:a])
-C2 = make_class(:C2, [], [:b, :c])
-C3 = make_class(:C3, [C1, C2], [:d])
-
-c1i1 = make_instance(C1, :a=>1)
-c2i1 = make_instance(C2, :b=>7, :c=>3)
-c2i2 = make_instance(C2, :b=>4, :c=>5)
-c3i1 = make_instance(C3, :b=>2)
-c3i2 = make_instance(C3, :b=>2, :a=>5)
-
-get_slot(c2i1, :b)
-set_slot!(c2i1, :b, 3)
-get_slot(c2i1, :b)
-get_slot(c3i2, :e)
+#foo(make_instance(C1))
+#foo(make_instance(C2, :b=>42))
